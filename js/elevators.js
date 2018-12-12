b27212e6945888d1974180987d54f6d693357984
Jose L Cuevas <jcuevas@mac.com>
2018-12-12 16:26:23 -0400

    save point

--- js/elevators.js
+++ js/elevators.js
@@ -3,3 +3,31 @@
  * Elevetors Manager and Controller
  */
 
+var helper = {
+	extend: function(a, b){
+		for(var key in b){
+			if(b.hasOwnProperty(key))
+				a[key] = b[key];
+		}
+		return a;
+	}
+}
+
+var elevator_manager = {
+	init: function(options){
+		console.log("@elevator_manager::init()");
+		this.consoleSel = options;
+		
+	},
+	createElevator: function(options){
+		var defaults = {
+			code: "RANDELV",
+			floorServiceStart: 1,
+			floorServiceEnd: 2,
+		};
+		
+	},
+	manageElevator: function(elv){
+		
+	}
+}
