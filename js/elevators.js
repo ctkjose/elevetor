5af04710fc58fd6f44ab927e5a82a6b86bd8d10d
Jose L Cuevas <jcuevas@mac.com>
2018-12-12 17:11:12 -0400

    save point

--- js/elevators.js
+++ js/elevators.js
@@ -35,9 +35,16 @@ var elevatorManager = {
 		for(var k in this.items){
 			var elv = this.items[k];
 			if(elv.status >= this.elevator.kStatusOutOfService) continue;
+			if(elv.location != location) continue;
+			elvs.push(elv);
 		}
 		
 		return elvs;
+	},
+	controlElevatorReachNextFloor: function(elv){
+		//Report travel and status
+		console.log("Reached next floor");
+		console.log(elv);
 	},
 	callButton: function(location, floor, dir){
 				
@@ -47,7 +54,7 @@ var elevatorManager = {
 		kStatusTraveling: 1,
 		kStatusOutOfService: 100,
 		kDirectionUp: 1,
-		kDirectionDown: 2,
+		kDirectionDown: -1,
 		create: function(options){
 			var defaults = {
 				code: "RANDELV",
@@ -58,10 +65,15 @@ var elevatorManager = {
 				
 		
 			var elv = {
+				manager: elevatorManager, 
 				status: this.kStatusOutOfService,
+				floor: 0,
 				travelDirection: this.kDirectionUp,
 				ops: helper.extend(defaults, options),
 			};
+			
+			elv.floor = elv.ops.floorServiceStart;
+			
 			this.decorate(elv);
 			return elv;
 				
@@ -71,8 +83,24 @@ var elevatorManager = {
 				obj[key] = this.fn[key];
 			}
 		},
+		
 		fn: {
-			
+			travel: function(){
+				var _this = this;
+				setTimeout(function(){
+					_this.reachNextFloor();
+				}, 200);
+			},
+			reachNextFloor: function(){
+				this.floor += this.travelDirection;
+				if(this.travelDirection == elevatorManager.elevator.kDirectionUp){
+					this.floor = Math.min(this.floor,  this.ops.floorServiceEnd);
+				}else{
+					this.floor = Math.max(this.floor,  this.ops.floorServiceStart);
+				}
+)
+				this.manager.controlElevatorReachNextFloor(this);
+			},
 			callButton: function(floor, dir){
 				
 			},
