99edec19b2ca50932c27c3b55fcbc76e7de535d4
Jose L Cuevas <jcuevas@mac.com>
2018-12-12 17:25:46 -0400

    save point

--- js/elevators.js
+++ js/elevators.js
@@ -16,6 +16,9 @@ var helper = {
 
 var elevatorManager = {
 	items: {},
+	options: {
+		maxTravelsBeforeService: 100,
+	},
 	init: function(options){
 		console.log("@elevator_manager::init()");
 		this.consoleSel = options;
@@ -46,8 +49,58 @@ var elevatorManager = {
 		console.log("Reached next floor");
 		console.log(elv);
 	},
+	controlElevatorReachFloor: function(elv){
+		//Reached floor
+		console.log("Reached requested floor");
+		console.log(elv);
+		
+		
+		elv.serviceCount++;
+		
+		if(elv.serviceCount == this.options.maxTravelsBeforeService ){
+			elv.status = this.elevator.kStatusOutOfService;
+		}else{
+			elv.status = this.elevator.kStatusIdle;
+		}
+	},
 	callButton: function(location, floor, dir){
-				
+		var elvs = this.getAvailableElevator(location);
+		
+		///TODO:save push log/statistics
+		if(elvs.length ==0 ){
+			console.log("No elevators available at location");
+			return undefined;
+		}
+		
+		//find closest elevator
+		var lastDist = 999999;
+		var bestElvIdx = -1;
+		var availableElvs = [];
+		for(var i=0; i< elvs.length; i++){
+			let e = elvs[i];
+			console.log("evaluating elv[%d] %o", i, e);
+			//for now dont consider diff travel direction as option
+			if(e.travelDirection != dir) continue;
+			if(dir == this.elevator.kDirectionUp){
+				if(e.floor > floor) continue;	
+			}else{
+				if(e.floor < floor) continue;
+			}
+			availableElvs.push(e);
+			
+			let dist = Math.abs(e.floor-floor);
+			if(dist < lastDist){
+				bestElvIdx = availableElvs.length-1;
+			}
+		}
+		
+		
+		//some bounds and error checking ommited
+		
+		if(bestElvIdx >= 0){
+			return availableElvs[bestElvIdx];
+		}
+		return undefined;
 	},
 	elevator: {
 		kStatusIdle: 0,
@@ -68,6 +121,8 @@ var elevatorManager = {
 				manager: elevatorManager, 
 				status: this.kStatusOutOfService,
 				floor: 0,
+				floorTarget: 0,
+				serviceCount: 0,
 				travelDirection: this.kDirectionUp,
 				ops: helper.extend(defaults, options),
 			};
@@ -101,6 +156,9 @@ var elevatorManager = {
 				
 				
 				this.manager.controlElevatorReachNextFloor(this);
+				if(this.floor == this.floorTarget){
+					this.manager.controlElevatorReachFloor(this);
+				}
 			},
 			callButton: function(floor, dir){
 				
